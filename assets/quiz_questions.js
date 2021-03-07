f06a8733e6ec4bb6899b13499ad280a29db0f51b
DaveBland <david.bland@gmx.co.uk>
2021-03-07 20:20:19 +0000

    *added data attr to control answer classes *moved start quiz question to top of page

--- assets/quiz_questions.js
+++ assets/quiz_questions.js
@@ -1,9 +1,8 @@
-/* Temporary message before the quiz starts*/
+/* Remove temporary message before the quiz starts*/
 
 function removeMessage() {
-  document.getElementById("message").innerHTML = " ";
+  document.getElementById("message").style.display = "none";
 }
-
 
 /* Picks up quiz data from JSON
 Generates random quiz rounds */
@@ -49,27 +48,24 @@ class QuizQuestions {
             alert("End of round");            
             return this.newRound();
         }
+
+        // Reset correct answer attributes & classes
+        for (let i=1; i < 5; i++) {            
+            document.getElementById(`answer${i}`).setAttribute("data-correctanswer","false");
+            document.getElementById(`answer${i}`).classList.remove("correct_answer","incorrect_answer");
+        }
+
         // Populate question and answer elements in the DOM, putting answers in random positions
         let qanda = this.questions.pop()
         let randomPosition = [1,2,3,4].sort(() => Math.random() - 0.5);
-        
-        // Remove correct answer class from previous question
-        if (document.getElementsByClassName("correct_answer").length > 0) {
-            document.getElementsByClassName("correct_answer")[0].classList.remove("correct_answer");                       
-        }
 
         document.getElementById("question").innerText = qanda['question'];
         let correctPos = randomPosition.pop();
         document.getElementById(`answer${correctPos}`).innerText = qanda['correct_answer'];
-        document.getElementById(`answer${correctPos}`).classList.add("correct_answer");
+        document.getElementById(`answer${correctPos}`).setAttribute("data-correctanswer","true");       
         document.getElementById(`answer${randomPosition.pop()}`).innerText = qanda['incorrect_answers'][0];
         document.getElementById(`answer${randomPosition.pop()}`).innerText = qanda['incorrect_answers'][1];
         document.getElementById(`answer${randomPosition.pop()}`).innerText = qanda['incorrect_answers'][2];
-
-        // Clear answer area css (temporary)
-        for (let i in document.getElementsByClassName("answer")) {            
-            document.getElementsByClassName("answer")[i].style.color = "white";
-        }
     }
 }
 
--- assets/quiz_results.js
+++ assets/quiz_results.js
@@ -3,16 +3,16 @@ var numCorrectThisRound = 0;
 function showResults(answerClicked, resultsContainerId){
 
 	// Check if answer clicked is correct answer (has css class 'correct_answer')
-	if (answerClicked.classList.contains("correct_answer")) {
+	if (answerClicked.getAttribute("data-correctanswer") == "true") {
 		numCorrectThisRound++;
 
 		// color the answer green
-		answerClicked.style.color = 'lightgreen';
+		answerClicked.classList.add("correct_answer")
 	}
 	// if answer is wrong
 	else {
 		// color the answers red
-		answerClicked.style.color = 'red';
+		answerClicked.classList.add("incorrect_answer")
 	}
 
 	// show number of correct answers out of total
